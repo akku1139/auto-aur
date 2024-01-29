f4b29ada63e914641db55f71e812355cf973537d
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-29 22:38:43 +0900

    Wip

---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+wget https://geo.mirror.pkgbuild.com/iso/latest/archlinux-bootstrap-x86_64.tar.gz
+tar xzf archlinux-bootstrap-x86_64.tar.gz --strip-components 1
+
+echo 'Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch' >> etc/pacman.d/mirrorlist
