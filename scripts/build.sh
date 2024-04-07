71af1d5e08686f8e87d48e53ea26c044a601da19
-akku- <151517253+akku1139@users.noreply.github.com>
2024-04-07 18:36:10 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,10 @@
+pacman -S --needed base-devel git
 
+mkdir work
+cd work
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/paru.tar.gz
+tar xf paru.tar.gz
+cd paru
+makepkg -sci
 
-pacman -Q > /tmp/packages
+repo-add /repo/repo.db.tar.gz paru.pkg.tar.zstd
