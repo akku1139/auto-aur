4a179e79c1faa00470d9fba746636492f21c1553
-akku- <151517253+akku1139@users.noreply.github.com>
2024-06-01 12:46:05 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,10 +14,6 @@ pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 pac -U "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst" \
        "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
-cat >> /etc/pacman.conf << EOL
-[chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist
-EOL
 
 # Setup CachyOS
 #pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
@@ -28,5 +24,14 @@ EOL
 #[cachyos]
 #Include = /etc/pacman.d/cachyos-mirrorlist
 #EOL
+
+cat >> /etc/pacman.conf << EOL
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+
+[aur]
+SigLevel = PackageOptional DatabaseOptional
+Server = file://${PWD}/repo
+EOL
 
 pac -Syu base-devel sudo paru
