13d9829aa7fcfb5c97b026c8f064134b66cbf6d0
-akku- <151517253+akku1139@users.noreply.github.com>
2024-06-01 17:31:41 +0900

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -50,7 +50,7 @@ LocalRepo = auto-aur
 PreBuildCommand = $PWD/scripts/prebuild.sh
 EOL
 
-mkdir --mode=777 -p /home/nobody
+mkdir --mode=755 -p /home/nobody
 chown nobody:nobody /home/nobody
 usermod -d /home/nobody nobody
 chmod -R 777 $PWD/repo
