569d8b1229d08fbfbaec1db39631de20b4a43420
-akku- <151517253+akku1139@users.noreply.github.com>
2024-06-01 16:48:45 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-# cat packages.txt | xargs sudo -u nobody paru --noconfirm -S
-cat packages.txt | xargs sudo paru --noconfirm -S
+cat packages.txt | xargs sudo -u nobody paru --noconfirm -S
