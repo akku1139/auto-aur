d051131e70d827b354fb0d09365b141bbd2bf9fa
-akku- <151517253+akku1139@users.noreply.github.com>
2024-06-01 14:46:09 +0900

    debug

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+echo "---------- Start prebuild.sh ----------"
+
 PKGNAME=`basename $PWD`
 
 PATCHDIR=$GITHUB_WORKSPACE/patches/$PKGNAME/
+echo "PATCHDIR:" $PATCHDIR
 
 if [ ! -e $PATCHDIR ]; then
   exit
@@ -12,3 +15,5 @@ for patch in `find $PATCHDIR -maxdepth 1 -type f | sort`; do
   echo "Applying the patch:" $patch
   patch -p1 < $patch
 fi
+
+echo "---------------------------------------"
