ddcc33e571c3f22010e649825dfa706579ae2e6f
-akku- <151517253+akku1139@users.noreply.github.com>
2024-06-01 11:35:02 +0900

    noconfirm

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,18 @@
 # Show commands
 set -x
 
-pacman -Sy --noconfirm
+# Alias
+shopt -s expand_aliases
+alias pac="pacman --noconfirm"
+
+pac -Sy
 pacman-key --init
 
 # Setup Chaotic AUR
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
-pacman -U "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst" \
-          "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
+pac -U "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst" \
+       "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
 cat > /etc/pacman.conf << EOL
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
@@ -17,13 +21,13 @@ EOL
 # Setup CachyOS
 pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key F3B607488DB35A47
-pacman -U "https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst" \
-          "https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-18-1-any.pkg.tar.zst"
+pac -U "https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst" \
+       "https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-18-1-any.pkg.tar.zst"
 cat > /etc/pacman.conf << EOL
 [cachyos]
 Include = /etc/pacman.d/cachyos-mirrorlist
 EOL
 
-pacman -Syu --noconfirm --needed base-devel git sudo paru
+pac -Syu --needed base-devel git sudo paru
 
 repo-add /repo/repo.db.tar.gz paru.pkg.tar.zstd
