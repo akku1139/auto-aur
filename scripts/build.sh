3dabb7f17b61b2933a3cfc0348545e04df3cba6a
-akku- <151517253+akku1139@users.noreply.github.com>
2024-06-01 18:11:53 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-# cat packages.txt | xargs sudo -u nobody paru --noconfirm -S
-# Show commands
-set -x
-
-# Alias
-shopt -s expand_aliases
-alias pac="pacman --noconfirm"
-alias pr="sudo -u nobody paru --noconfirm --clonedir /tmp/buildpkg"
-
-pr -S alsaequal
+cat packages.txt | xargs sudo -u nobody paru --noconfirm -S
