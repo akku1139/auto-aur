020829afbad70f62d5761d8bd45ce3530f394337
-akku- <151517253+akku1139@users.noreply.github.com>
2024-06-01 19:23:12 +0900

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,7 +18,8 @@ pac -U "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst" \
        "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
 
 cd repo
-ln -s auto-aur.db.tar.gz auto-aur.db
+# touch auto-aur.db.tar.gz
+# ln -s auto-aur.db.tar.gz auto-aur.db
 chown -R nobody:nobody .
 cd ..
 
