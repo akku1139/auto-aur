9afc298af448b829db68462090274c5c55d04e40
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-12 20:34:11 +0900

    Update prebuild.sh

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -2,16 +2,14 @@
 
 set -ex
 
-pwd
-ls -lh
-find
-
 PKGNAME=`basename $PWD`
 PATCHDIR=`dirname $0`/../patches/$PKGNAME/
 
 if [ ! -e $PATCHDIR ]; then
   exit
 fi
+
+makepkg --nobuild
 
 for patch in `find $PATCHDIR -maxdepth 1 -type f | sort`; do
   echo "Applying the patch:" $patch
