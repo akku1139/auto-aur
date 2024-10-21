ceae7046b1ecdd1cc20087202b9cd94ef393d34f
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-21 23:07:29 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -ex
 
-sudo -u nobody paru
+sudo -u builder paru
 
 if [ -e packages.txt ]; then
-  xargs -a packages.txt sudo -u nobody paru --noconfirm --nocheck --nocleanafter -S
+  xargs -a packages.txt sudo -u builder paru --noconfirm --nocheck --nocleanafter -S
   rm packages.txt
 fi
