134bf3efd3cd550586ac89169dc864ab90839a92
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-29 11:18:44 +0900

    fix find

--- scripts/pre-deploy-cf.sh
+++ scripts/pre-deploy-cf.sh
@@ -2,7 +2,7 @@
 set -ex
 cd public
 
-for file in `find -size +25M type f`; do
+for file in `find -size +25M -type f`; do
   rm $file
   echo "$file" "https://akku1139.github.io/auto-aur/$file" 302 >> _redirects
 done
