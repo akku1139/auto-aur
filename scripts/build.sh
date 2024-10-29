42b15d8257ba1edc119dd5d66fbf83ea8b03c9c1
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-29 15:44:51 +0900

    ?

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -ex
 
-yes "" | sudo -u builder paru --noconfirm
+# yes "" | sudo -u builder paru --noconfirm
+sudo -u builder paru --noconfirm
 
 if [ -e packages.txt ]; then
   yes "" | xargs -a packages.txt sudo -u builder paru --noconfirm --nocheck --nocleanafter -S
