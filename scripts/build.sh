9932f32553e8c8457d5512a0a96836a3ecb78342
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-18 21:37:58 +0900

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,31 +10,32 @@ sudo -u builder paru
 if [ -e packages.txt ]; then
   # xargs -a packages.txt sudo -u builder paru --noconfirm --nocheck --nocleanafter -S
   for pkg in $(cat packages.txt); do
-    case `echo $p | cut -c -4` in
+    if [ "$pkg" ]
+    case `echo "$pkg" | cut -c -4` in
       git:)
-        repo=$( echo $pkg | cut -c 5- )
-        confdir=$(cd "non-aur/git-$( echo $repo | base64 )" && pwd)
+        repo=$( echo "$pkg" | cut -c 5- )
+        confdir="non-aur/git-$( echo "$repo" | base64 )"
 
-        lc=$( git ls-remote -qh $repo | cut -f1 )
+        lc=$( git ls-remote -qh "$repo" | cut -f1 )
 
-        if [! -d  $confdir ]; then
-          mkdir $confdir
-          echo $lc > $confdir/latest-commit
+        if [ ! -d "$confdir" ]; then
+          mkdir "$confdir"
+          echo "$lc" > "$confdir/latest-commit"
         fi
 
-        if [ $lc != $( echo $confdir/latest-commit ) ]; then
+        if [ "$lc" != $( echo "$confdir/latest-commit" ) ]; then
           echo $lc > $confdir/latest-commit
           workdir=$( cd $( mktemp --directory --tmpdir=work ) && pwd )
-          cd $workdir
-          git clone --depth=1 $repo .
+          cd "$workdir"
+          git clone --depth=1 "$repo" .
           sudo -u builder paru -U
         fi
         ;;
       *)
-        sudo -u builder paru --noconfirm --nocheck --nocleanafter -S $pkg
+        sudo -u builder paru --noconfirm --nocheck --nocleanafter -S "$pkg"
         ;;
     esac
-    cd $basepath
+    cd "$basepath"
   done
   rm packages.txt
 fi
