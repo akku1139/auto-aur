7dfefb11dc3cb98425676e2aacdeb0f6d606cc55
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-20 16:40:31 +0900

    Update tidyup.sh

--- scripts/tidyup.sh
+++ scripts/tidyup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "**** starting tidyup.sh ****"
 # https://github.com/NobuoTsukamoto/my_actions_test/blob/main/.github/workflows/test_contains.yml
+npm uninstall bazel || true
+npm uninstall bazelisk || true
+rustup self uninstall -y
 apt purge \
   ansible \
   aria2 \
@@ -96,23 +99,82 @@ dpkg -r packages-microsoft-prod　> /dev/null 2>&1 || true
 apt-get autoremove -y > /dev/null 2>&1 || true
 apt-get autoclean -y > /dev/null 2>&1 || true
 apt list --installed
-rm -rf /home/work/* /var/* /opt/* /usr/local || true
+rm -rf "/home/work/*" || true
+rm -rf "/opt/*" || true
 rm -rf "/usr/share/dotnet" || true
 rm -rf "$AGENT_TOOLSDIRECTORY" || true
+rm -rf "/usr/local/lib/android" || true
+rm -rf "/usr/local/share/boost" || true
 rm -rf "/usr/lib/jvm" || true
 rm -rf "/usr/share/swift" || true
+rm -rf "/usr/local/julia*" || true
 rm -rf "/usr/share/kotlinc" || true
+rm -rf "/usr/local/share/edge_driver" || true
+rm -rf "/usr/local/share/chromedriver-linux64" || true
+rm -rf "/usr/local/share/gecko_driver" || true
 rm -rf "/usr/share/miniconda" || true
+rm -rf "/usr/local/share/phantomjs*" || true
 rm -rf "/usr/share/sbt" || true
+rm -rf "/usr/local/sqlpackage" || true
 rm -rf "/usr/bin/yq" || true
+rm -rf "/usr/local/share/vcpkg" || true
+rm -rf "/usr/local/bin/terraform" || true
+rm -rf "/usr/local/bin/stack" || true
+rm -rf "/usr/local/bin/aliyun" || true
+rm -rf "/usr/local/bin/pulumi" || true
+rm -rf "/usr/local/bin/pulumi-language-dotnet" || true
+rm -rf "/usr/local/bin/azcopy" || true
+rm -rf "/usr/local/aws-cli" || true
+rm -rf "/usr/local/bin/bicep" || true
+rm -rf "/usr/local/bin/rebar3" || true
+rm -rf "/usr/local/bin/phpunit" || true
+rm -rf "/usr/local/bin/packer" || true
+rm -rf "/usr/local/bin/bicep" || true
+rm -rf "/usr/local/bin/docker-compose" || true
 rm -rf "/home/.dotnet/tools" || true
 rm -rf "/etc/skel/.dotnet/tools" || true
+rm -rf "/usr/local/bin/minikube" || true
+rm -rf "/usr/local/bin/kustomize" || true
+rm -rf "/usr/local/bin/kubectl" || true
+rm -rf "/usr/local/bin/kind" || true
+rm -rf "/usr/local/bin/helm" || true
 rm -rf "/home/linuxbrew" || true
 rm -rf "/usr/share/apache-maven-3.8.8" || true
 rm -rf "/usr/share/gradle-8.7" || true
+rm -rf "/usr/local/share/phantomjs-8.6" || true
+rm -rf "/usr/local/bin/azcopy_11.3.1" || true
+rm -rf "/usr/local/bin/bicep" || true
+rm -rf "/usr/local/bin/.ghcup" || true
+rm -rf "/usr/local/.ghcup" || true
 rm -rf "/home/.nvm" || true
 rm -rf "/usr/bin/composer" || true
+rm -rf "/usr/local/bin/phpunit" || true
+rm -rf "/usr/local/bin/pulumi-analyzer-policy" || true
+rm -rf "/usr/local/bin/pulumi-analyzer-policy-python" || true
+rm -rf "/usr/local/bin/pulumi-language-java" || true
+rm -rf "/usr/local/bin/pulumi-language-nodejs" || true
+rm -rf "/usr/local/bin/pulumi-language-go" || true
+rm -rf "/usr/local/bin/pulumi-language-python" || true
+rm -rf "/usr/local/bin/pulumi-language-python-exec" || true
+rm -rf "/usr/local/bin/pulumi-language-yaml" || true
+rm -rf "/usr/local/bin/pulumi-resource-pulumi-nodejs" || true
+rm -rf "/usr/local/bin/pulumi-resource-pulumi-python" || true
+rm -rf "/usr/local/bin/pulumi-watch" || true
+rm -rf "/usr/local/bin/oc" || true
+rm -rf "/usr/local/bin/ctest" || true
+rm -rf "/usr/local/bin/cmake-gui" || true
+rm -rf "/usr/local/bin/ccmake" || true
+rm -rf "/usr/local/bin/cpack" || true
+rm -rf "/usr/local/bin/oras" || true
+rm -rf "/usr/local/share/emacs" || true
+rm -rf "/usr/local/share/fonts" || true
+rm -rf "/usr/local/share/icons" || true
+rm -rf "/usr/local/share/man" || true
+rm -rf "/usr/local/share/chromium" || true
+rm -rf "/usr/local/share/powershell" || true
+rm -rf "/usr/local/share/doc" || true
 rm -rf "/home/perflog" || true
 rm -rf "/usr/libexec/docker/cli-plugins" || true
 rm -rf "/usr/bin/docker-credential-ecr-login" || true
+rm -rf "/usr/local/lib/lein" || true
 echo "**** tidyup.sh done ****"
