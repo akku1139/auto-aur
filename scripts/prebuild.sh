3cf43dc0c51e211043b9b3eb597d72b37e1b731f
-akku- <151517253+akku1139@users.noreply.github.com>
2024-11-24 00:11:20 +0900

    fix

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -3,31 +3,28 @@
 #set -ex
 set -e
 
-BASEDIR=`dirname $0`
 PKGNAME=`basename $PWD`
-PATCHDIR="$BASEDIR/../patches/$PKGNAME"
+PATCHDIR="$( dirname $0 )/../patches/$PKGNAME"
 
 if [ ! -d $PATCHDIR ]; then
   exit
 fi
-
-workdir=$( cd $( mktemp --directory --tmpdir="$BASEDIR/work" ) && pwd )
 
 if [ -f $PATCHDIR/deps ]; then
   sudo pacman --noconfirm -S $( cat $PATCHDIR/deps )
 fi
 
 targetver=$( awk '$1=="pkgver" {v=$3} $1=="pkgrel" {r=$3} $1=="epoch" {e=$3":"} END {print e v "-" r}' .SRCINFO )
-patchver= $( cat "$PATCHDIR/version" )
+patchver=$( cat "$PATCHDIR/version" )
 if [ $targetver != $patchver ]; then
   echo "Error (prebuild.sh): Patch version and package version are different"
   echo "Package: $targetver"
-  echo "Patch: patchver"
+  echo "Patch: $patchver"
   echo "Exit."
   exit 1
 fi
 
-for patch in `find $PATCHDIR/pre/ -maxdepth 1 -type f | sort`; do
+for patch in $( find $PATCHDIR/pre/ -maxdepth 1 -type f | sort ); do
   echo "Applying a patch:" $patch
   patch -p1 < $patch
 done
