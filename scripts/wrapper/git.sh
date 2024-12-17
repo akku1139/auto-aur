4cf72d5c464143e305c7fffd65964aee8746ea28
-akku- <151517253+akku1139@users.noreply.github.com>
2024-12-17 23:45:33 +0900

    Update and rename git to git.sh

--- scripts/wrapper/git.sh
+++ scripts/wrapper/git.sh
@@ -1,13 +1,25 @@
+***** This is very wip *****
+***** DO NOT USE *****
+
 #!/bin/sh
 
 # https://github.com/Morganamilo/paru/issues/1027#issuecomment-1691320232
 
-#echo "***** Git Wrapper *****" > /dev/stderr
-#set -ex
+pre=()
+
+for arg in "$@"; do
+  if [[ "$arg" != -* ]]; then
+    break
+  fi
+  pre+=("$arg")
+  shift
+done
+
+echo $1
 
 if [ "$1" = clone ]; then
   add="--filter=blob:none"
 fi
 
 # https://qiita.com/Ping/items/57fd75465dfada76e633#---%E3%81%AE%E9%81%95%E3%81%84
-exec /usr/bin/git "$1" $add "${@:2}"
+exec /usr/bin/git "$pre" "$1" $add "${@:2}"
