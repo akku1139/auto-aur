a5cf12fae04bc84c41d65cf27b8c56359ede7ff8
-akku- <akkun11.open@gmail.com>
2025-03-01 00:08:23 +0900

    save2

---
+++ scripts/git-save.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+cd "$( dirname $0 )/../"
+if [ -n "$(git status --porcelain)" ]; then
+  git pull
+  python scripts/genui.py
+  git add -A
+  git commit -m "Data"
+  git push
+fi
