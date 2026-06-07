cbb33f0080baf4a40ef2ec08300af6ea2b87b060
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-06-07 22:51:09 +0900

    aaa

--- scripts/update_repo_data.py
+++ scripts/update_repo_data.py
@@ -95,15 +95,15 @@ def main():
     if sig_file.exists():
         shutil.copy(sig_file, db_path.with_suffix(db_path.suffix + '.sig'))
 
-    files_tar_gz = temp_dir / 'auto-aur.files.tar.gz'
-    if files_tar_gz.exists() and args.gpg_key:
-        subprocess.run([
-            'gpg', '--batch', '--detach-sign', '--local-user', args.gpg_key,
-            str(files_tar_gz)
-        ], check=True)
-        # 署名ファイルもコピー先に含める
-        shutil.copy(files_tar_gz.with_suffix(files_tar_gz.suffix + '.sig'),
-                    db_path.with_suffix('').with_suffix('.files.tar.gz.sig'))
+    # files_tar_gz = temp_dir / 'auto-aur.files.tar.gz'
+    # if files_tar_gz.exists() and args.gpg_key:
+    #     subprocess.run([
+    #         'gpg', '--batch', '--detach-sign', '--local-user', args.gpg_key,
+    #         str(files_tar_gz)
+    #     ], check=True)
+    #     # 署名ファイルもコピー先に含める
+    #     shutil.copy(files_tar_gz.with_suffix(files_tar_gz.suffix + '.sig'),
+    #                 db_path.with_suffix('').with_suffix('.files.tar.gz.sig')) # この行間違ってる
 
     # Generate _redirects file
     repo = os.environ.get('GITHUB_REPOSITORY', 'unknown/repo')
