8bbe578ffd0bb8af050d3e2416a3d384003be156
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-06-09 22:53:16 +0900

    skip source signature check for coreboot-utils

--- scripts/build/entry.sh
+++ scripts/build/entry.sh
@@ -4,5 +4,5 @@ set -ex
 pacman -Sy
 chown -R builder:builder /home/builder/src
 cd /home/builder/src
-sudo -u builder makepkg -s --noconfirm
+sudo -u builder makepkg -s --noconfirm --skippgpcheck
 cp *.pkg.tar.zst /repo/
